372dcdc64583d362df99cf97f4631ebda757c483
Arsen <arcenk3x@mail.ru>
2023-06-28 19:27:35 +0600

    feat: add mobile statusbar color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#04000d" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#04000d" />
+      </Head>
       <body>
         <Main />
         <NextScript />
